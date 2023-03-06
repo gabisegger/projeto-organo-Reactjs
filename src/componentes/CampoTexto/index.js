e6f04d1dceee836d57ff5630be78f5a998de9558
gabisegger <gabi.segger@gmail.com>
2023-03-06 20:32:17 -0300

    Criação do evento onChange() no componente CampoTexto

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    let valor = '';
+
+    const aoDigitado = (e) => {
+        valor = e.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
