d32254b7123eaa49a45f53b75d1ec7018fc63f3e
gabisegger <gabi.segger@gmail.com>
2023-03-07 20:31:43 -0300

    Utilizando o useState no formulário

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,17 +2,14 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    let valor = '';
-
     const aoDigitado = (e) => {
-        valor = e.target.value
-        console.log(valor)
+        props.aoAlterado(e.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,7 @@ const ListaSuspensa = (props) => {
     return (
         <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select required={props.required}>
+            <select onChange={e => props.aoAlterado(e.target.value)} required={props.required} value={props.valor}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
