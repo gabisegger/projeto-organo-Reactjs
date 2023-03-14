d63e446517f00a15732c56a158240916741a5de3
gabisegger <gabi.segger@gmail.com>
2023-03-14 20:57:22 -0300

    Criação do componente Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/gabisegger.png' alt='Gabriela Segger'/>
+            </div>
+            <div className='rodape'>
+                <h4>Gabriela S. Caruso</h4>
+                <h5>Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
