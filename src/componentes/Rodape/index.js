b6d6e8d912792d5f2d87d1cd3e5faa9054bf9c3a
gabisegger <gabi.segger@gmail.com>
2023-03-28 19:43:55 -0300

    Footer site

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,13 +1,14 @@
 import './Rodape.css'
 
-const Rodape = () => {
+const Rodape = (props) => {
     return(
         <>
-            <footer className="Rodape">
+            <footer className="footer">
+                <img className="background" src="/imagens/fundoFooter.png" alt="Background"/>
                 <div className="redesSociais">
-                    <a><img src="/imagens/facebook.png" alt="Facebook"/></a>
-                    <a><img src="/imagens/twitter.png" alt="Twitter"/></a>
-                    <a><img src="/imagens/instagram.png" alt="Instagram"/></a>
+                    <a href={props.facebook}><img src="/imagens/facebook.png" alt="Facebook"/></a>
+                    <a href={props.twitter}><img src="/imagens/twitter.png" alt="Twitter"/></a>
+                    <a href={props.instagram}><img src="/imagens/instagram.png" alt="Instagram"/></a>
                 </div>
                 <img src="/imagens/Logo.png" alt="Logo Organo"/>
                 <p className="creditos">Desenvolvido por Alura.</p>
