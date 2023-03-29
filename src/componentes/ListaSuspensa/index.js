aa6996574aaa82f4f8872543a06983bac0336617
gabisegger <gabi.segger@gmail.com>
2023-03-29 13:55:58 -0300

    Ajustes no código

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,6 +5,7 @@ const ListaSuspensa = (props) => {
         <div className="lista-suspensa">
             <label>{props.label}</label>
             <select onChange={e => props.aoAlterado(e.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
